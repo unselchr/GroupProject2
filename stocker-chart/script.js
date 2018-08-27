0737124f6204e8d04fd58d0ae57e854b41356f96
rschwartz <37844284+randallwschwartz@users.noreply.github.com>
2018-08-27 17:40:40 -0500

    rws 8/27 updates to add API call

--- stocker-chart/script.js
+++ stocker-chart/script.js
@@ -1,3 +1,4 @@
+// Script for creating the Graph using chart.js
 // Our labels along the x-axis
 var months = [0,1,2,3,4,5,6];
 // For drawing the lines
@@ -25,3 +26,27 @@ var myChart = new Chart(ctx, {
     ]
   }
 });
+
+
+// Script for requesting a quote on September feeder calves from the Barchart API
+//
+
+function requestQuote(twoDigitYear) {
+
+  // Barchart API call where the symbol for September 2018 feeder calves is hardwired in 
+  var queryURL = "https://marketdata.websol.barchart.com/getQuote.json?apikey=fb979bafe708bfae93cf3f7537d8b896&symbols=GFU18"
+  
+  // alternative API call if the current year is provided in variable twoDigitYear
+  // var queryURL = "https://marketdata.websol.barchart.com/getQuote.json?apikey=fb979bafe708bfae93cf3f7537d8b896&symbols=GFU" + twoDigitYear
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function(response) {
+    
+      var price = response.results.lastPrice;
+      console.log(price);
+    }
+  );
+}
