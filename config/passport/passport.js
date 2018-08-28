a8a261ad8c78fef488dd9755776830ccdb29f934
unselchr <cdojrf@gmail.com>
2018-08-27 19:54:41 -0500

    serializing done

--- config/passport/passport.js
+++ config/passport/passport.js
@@ -58,6 +58,19 @@ module.exports = function(passport, user) {
           }
         });
       }
-    )
+    ),
+    passport.serializeUser(function(user, done) {
+      done(null, user.id);
+    }),
+    // deserialize user
+    passport.deserializeUser(function(id, done) {
+      User.findById(id).then(function(user) {
+        if (user) {
+          done(null, user.get());
+        } else {
+          done(user.errors, null);
+        }
+      });
+    })
   );
 };
