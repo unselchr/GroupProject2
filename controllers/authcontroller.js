3c6b0c97695620a6e59608eb04ee4d0fb0337420
unselchr <cdojrf@gmail.com>
2018-09-01 05:07:33 -0500

    creating plans now works

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,5 +1,6 @@
-var user = require("../models/user");
-var plan = require("../models/plans");
+var models= require("../models");
+var user = models.user;
+var plan = models.Plans;
 var exports = (module.exports = {});
 exports.dashboard = function(req, res) {
   res.render("dashboard");
@@ -25,8 +26,29 @@ exports.plan = function(req, res) {
   res.render("plan", {});
 };
 exports.newPlan = function(req, res) {
-  res.render("/newPlan");
+  res.render("newPlan");
 };
 exports.myID = function(req, res) {
   res.json({id:req.session.passport.user});
+}
+exports.postNewPlan = function(req, res) {
+  //takes a json object with keys title,heads,rentPerHead,startingWeight,costPerHundredLbs,acresPerHead,otherCostPerHead,interestRate,lbsPerDay,daysOnPasture,vetCostPerHead,transportPerHead,userID,futurePrice
+  var newPlan=req.body;
+  plan.create({
+    planTitle:newPlan.title,
+    numHeadsPurchased:newPlan.heads,
+    aveStartingWeight:newPlan.startingWeight,
+    costPerHeadPer100Pounds:newPlan.costPerHundredLbs,
+    isManualCPHP100:true,
+    pastureAcresPerHead:newPlan.acresPerHead,
+    pastureRentPerHead:newPlan.rentPerHead,
+    vetCostPerHead:newPlan.vetCostPerHead,
+    truckTripPerHead:newPlan.transportPerHead,
+    interestRate:newPlan.interestRate,
+    weightGainPerDay:newPlan.lbsPerDay,
+    numDaysOnPasture:newPlan.daysOnPasture,
+    pricePerHeadPer100Pounds:newPlan.futurePrice,
+    userId:newPlan.userID
+  })
+  res.redirect("/dashboard");
 }
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -25,7 +25,8 @@ module.exports = function(app, passport) {
   app.get("/allPlans", isLoggedIn, authController.allPlans); //gets all plans by a users id and sends it as json
   app.get("/plan:planID", isLoggedIn, authController.plan); //gets plan by id and sends it as json
   app.get("/newPlan", isLoggedIn, authController.newPlan); //renders new plan page
-  app.get("/myID",isLoggedIn,authController.myID)
+  app.get("/myID",isLoggedIn,authController.myID);
+  app.post("/newPlan",isLoggedIn,authController.postNewPlan);
   //must be last
   function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
