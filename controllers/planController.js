5b06980db0208bd40608c506f03b123278918f00
unselchr <cdojrf@gmail.com>
2018-09-04 18:16:20 -0500

    fixed plancontroller formatting

--- controllers/planController.js
+++ controllers/planController.js
@@ -1,30 +1,32 @@
 var models = require("../models");
-var user = models.user;
+//var user = models.user;
 var plan = models.Plans;
 var exports = (module.exports = {});
 exports.allPlans = function(req, res) {
-    plan.findAll({
-        where: {
-            userId: req.session.passport.user
-        }
-    }).then(function(data) {
-        res.json(data);
+  plan
+    .findAll({
+      where: {
+        userId: req.session.passport.user
+      }
     })
+    .then(function(data) {
+      res.json(data);
+    });
 };
 exports.onePlan = function(req, res) {
-    plan.findAll({
-        where: {
-            id: req.params.planID.replace(/\D/g,""),
-            userId: req.session.passport.user
-        }
-    }).then(function(data){
-        //console.log("\n\n"+data+"\n\n");
-        if(data==null || data=={}){
-            res.json(data);
-        }
-        else{
-            res.render("404");
-        }
-        
+  plan
+    .findAll({
+      where: {
+        id: req.params.planID.replace(/\D/g, ""),
+        userId: req.session.passport.user
+      }
     })
+    .then(function(data) {
+      //console.log("\n\n"+data+"\n\n");
+      if (data === null || data === {}) {
+        res.json(data);
+      } else {
+        res.render("404");
+      }
+    });
 };
