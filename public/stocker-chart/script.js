6404a537df7816a8e1b21dc1c945d95dee6ddee9
rschwartz <37844284+randallwschwartz@users.noreply.github.com>
2018-09-05 07:55:55 -0500

    rws updates to plan page

--- public/stocker-chart/script.js
+++ public/stocker-chart/script.js
@@ -15,23 +15,25 @@ $.ajax({
   //--------------------------
   //
   // 1. Cost Per Head to Purchase Each Calf
-  var purchaseCostPerHead = parseFloat(response[0].aveStartingWeight) * parseFloat(response[0].costPerHeadPer100Pounds);
-  console.log(purchaseCostPerHead);
+  var purchaseCostPerHead = parseFloat(response[0].aveStartingWeight) * parseFloat(response[0].costPerHeadPer100Pounds) * 0.01;
+  console.log("purchaseCostPerHead: $" + purchaseCostPerHead);
   //
   // 2. Sum of Purchase Cost Per Head and Other Costs Per Head
-    var sumCostPerHead = purchaseCostPerHead + parseFloat(response[0].vetCostPerHead) + parseFloat(response[0].truckTripPerHead);
-    console.log(sumCostPerHead);
+    var sumCostPerHead = purchaseCostPerHead + parseFloat(response[0].pastureRentPerHead) + parseFloat(response[0].vetCostPerHead) + parseFloat(response[0].truckTripPerHead);
+    console.log("sumCostPerHead: " + sumCostPerHead);
   //
   // 3. Calculate Interest Cost Per Head
-    var interestCostPerHead = parseFloat(response[0].interestRate) * purchaseCostPerHead;
-  //
+    var interestCostPerHead = parseFloat(response[0].interestRate) * 0.01 * sumCostPerHead / 2;
+    console.log("interestCostPerHead: " + interestCostPerHead);
+    //
   // 4. Calculate Total Cost Per Head by Adding the Sum Per Head above and the Interest Cost Per Head
     var totalCostPerHead = sumCostPerHead + interestCostPerHead;
-    console.log(totalCostPerHead);
-    $("#totalCostPerHead").text(totalCostPerHead);
+    console.log("totalCostPerHead: " + totalCostPerHead);
+    $("#totalCostPerHead").text("$" + totalCostPerHead);
     //
   // 5. Calculate the Total Cost
     var totalCost = parseFloat(response[0].numHeadsPurchased) * totalCostPerHead;
+    $("#totalCost").text("$" + totalCost);
   //
   // 6. Total Weight Gain Per Head
     var totalWeightGainPerHead = parseFloat(response[0].weightGainPerDay) * parseFloat(response[0].numDaysOnPasture);
@@ -41,16 +43,18 @@ $.ajax({
     $("#finalWeightPerHead").text(finalWeightPerHead);
   //
   // 7. Total Sale Price Per Head is the final weight times the sale price per hundred pounds 
-    var totalSalePricePerHead = finalWeightPerHead * parseFloat(response[0].pricePerHeadPer100Pounds);
-    $("#salePricePerHead").text(totalSalePricePerHead);
+    var salePricePer100 = parseFloat(response[0].pricePerHeadPer100Pounds);
+    var totalSalePricePerHead = finalWeightPerHead * salePricePer100 * 0.01;
+    $("#salePricePer100").text("$" + salePricePer100);
+    $("#salePricePerHead").text("$" + totalSalePricePerHead);
   //
   // 8. Total (Sale) Price
     var totalPrice = parseFloat(response[0].numHeadsPurchased) * totalSalePricePerHead;
-    $("#totalSalePrice").text(totalPrice);
+    $("#totalSalePrice").text("$" + totalPrice);
   //
   // 9. Total Margin
     var totalMargin = totalPrice - totalCost;
-    $("#totalMargin").text(totalMargin);
+    $("#totalMargin").text("$" + totalMargin);
   //
 
   
